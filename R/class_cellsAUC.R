aa689c53f6916b57f5e7e62feb9e1d24c96ce644
u0103631 <u0103631@gbw-s-seq07.luna.kuleuven.be>
2017-03-08 02:24:39 +0100

    Minor improvements to cellsAUC-class

--- R/class_cellsAUC.R
+++ R/class_cellsAUC.R
@@ -1,15 +1,27 @@
 
 #' @title Class to store AUC
 #' @description Class with only one slot: @AUC, a matrix which contains the AUC for the gene-sets & cells.
-#' @example
-#' fakeAUC <- motifsAUC(AUC=matrix(1/1:30, nrow=3)) # gene-sets by cells.
+#' @examples
+#' fakeAUC <- cellsAUC(AUC=matrix(1/1:30, nrow=3)) # gene-sets by cells.
 #' fakeAUC
-#' dim(fakeAUC@AUC)
-#' class(fakeAUC@AUC)
-#' fakeAUC@AUC[,1:5]
-#' auc(fakeAUC)[,1:5]
+#'
+#' nGeneSets(fakeAUC)
+#' nCells(fakeAUC)
 #' cellNames(fakeAUC)
 #' geneSetNames(fakeAUC)
+#'
+#' class(fakeAUC)
+#' class(fakeAUC@AUC)
+#' dim(fakeAUC@AUC)
+#'
+#' # To subset & access the AUC slot:
+#' fakeAUC[1:2,]
+#' fakeAUC[,3:4]
+#' auc(fakeAUC)[,1:5] # fakeAUC@AUC[,1:5]
+#'
+#' # To subset the AUC slot, but keeping the class:
+#' subset(fakeAUC, 1:2, "geneSets")
+#' subset(fakeAUC, 3:4, "cells")
 #' @export
 
 cellsAUC <- setClass(
@@ -29,6 +41,23 @@ setMethod("show",
                           nrow(object@AUC), "gene-sets and", ncol(object@AUC), "cells"))
             if(!is.null(rownames(object@AUC))) message(paste("First gene-sets:", paste(head(rownames(object@AUC), 3), collapse=", ")))
             if(!is.null(colnames(object@AUC))) message(paste("First cells:", paste(head(colnames(object@AUC), 3), collapse=", ")))
+          }
+)
+
+setMethod('[', signature(x="cellsAUC"),
+          definition=function(x, i, j, drop){
+              x@AUC[i,j, drop=drop]
+          })
+
+setMethod("subset",
+          signature="cellsAUC",
+          definition = function(x, elements, select=c("geneSets", "cells")[1]) {
+              if(length(select) > 1) stop()
+
+              if(grepl("geneset", tolower(select))) x@AUC <- x@AUC[elements,, drop=FALSE]
+              if(grepl("cell", tolower(select))) x@AUC <- x@AUC[,elements, drop=FALSE]
+
+              x
           }
 )
 
@@ -58,3 +87,23 @@ setMethod("cellNames",
             colnames(object@AUC)
           }
 )
+
+#' @export
+setGeneric(name="nGeneSets", def=function(object) standardGeneric("nGeneSets"))
+setMethod("nGeneSets",
+          signature="cellsAUC",
+          definition = function(object) {
+              nrow(object@AUC)
+          }
+)
+
+#' @export
+setGeneric(name="nCells", def=function(object) standardGeneric("nCells"))
+setMethod("nCells",
+          signature="cellsAUC",
+          definition = function(object) {
+              ncol(object@AUC)
+          }
+)
+
+
