33a3ce01e848705719ba255f46f5b4c15ada1beb
vsc31245 <vsc31245@r23i27n22.genius.hpc.kuleuven.be>
2021-01-26 12:44:37 +0100

    1.13.1: Add function plotEmb_rgb

---
+++ R/aux_plotEmb_rgb.R
@@ -0,0 +1,93 @@
+#' @title plotEmb_rgb
+#' @description Colors the embeddings (t-SNE/Umap) based on the activity of 3 (groups of) geneSets
+#' @param mat4col AUC matrix (continuous or binary)
+#' @param embedding AUC matrix (continuous or binary)
+#' @param geneSetNames Gene sets to plot
+#' @param aucType "AUC" or "Binary"
+#' @param aucMaxContrast To increase the AUC contrast decrease the value.
+#' @param offColor Color por the cells completelly off. To deactivate (color as black), set to NULL.
+#' @param showPlot Whether to plot the coloured embeddings.
+#' @param showLegend Whether to plot add a legend to the plot.
+#' @param ... Other arguments to pass to the \code{plot} function.
+#' @return The cell colors (invisible)
+#' @examples 
+# par(mfrow=c(1,2))
+# 
+# setNames <- c("Dlx1", "Sox8")
+# cellCol <- plotEmb_rgb(mat2col, emb, setNames, aucType="AUC", aucMaxContrast=0.6)
+# text(-5,-23, attr(cellCol,"red"), col="red", cex=.7)
+# text(-10,-18, attr(cellCol,"green"), col="green", cex=.7)
+# 
+# setNames <- list(red=c("Dlx1","Dlx5"),
+#                      green=c("Neurod1"),
+#                      blue=c("Sox8"))
+# cellCol <- plotEmb_rgb(mat2col, emb, setNames, aucType="Binary")
+
+#' @export
+plotEmb_rgb <- function(mat4col, embedding, geneSetNames, aucType, 
+                         aucMaxContrast, offColor, 
+                         showPlot, showLegend, ...)
+{
+  # Check format
+  if(length(geneSetNames)>3) stop("To plot more than three regulons, group them by color.")
+  if(any(!names(geneSetNames) %in% c("red","green", "blue"))) 
+    stop('If a list, the names of geneSetNames should be "red","green", and/or"blue".')
+  if(aucMaxContrast<=0 | aucMaxContrast>1) stop("aucMaxContrast should be between 0 and 1")
+  
+  
+  if(is.null(names(geneSetNames)) && length(geneSetNames)<=3) names(geneSetNames) <- c("red","green", "blue")[seq_along(geneSetNames)]
+  
+  mat4col <- mat4col[onlyNonDuplicatedExtended(rownames(mat4col)),,drop=FALSE]
+  reguCols <- lapply(geneSetNames, function(x) unlist(sapply(x, function(tf) {
+    rownames(mat4col)[which(gsub("_extended","",getTF(rownames(mat4col))) %in% tf)] 
+  }
+  )))
+  reguCols <- reguCols[lengths(reguCols)>0]
+  
+  # Average of binary...
+  if(aucType=="binary")
+    cellColChan <- sapply(reguCols, function(modsCol) apply(mat4col[modsCol,, drop=FALSE], 2, mean))
+  # Color if all modules are "on"
+  # cellColChan <- sapply(reguCols, function(modsCol) apply(mat4col[modsCol,, drop=FALSE], 2, function(x) as.numeric(sum(x)==length(x))))
+  
+  # AUC
+  if(aucType=="auc") {
+    cellColChan <- sapply(reguCols, function(regCol) {
+      aucAvg <- colMeans(mat4col[regCol,, drop=FALSE])
+      setNames(sapply(as.numeric(aucAvg/(max(aucAvg)*aucMaxContrast)), min, 1), names(aucAvg))
+    })
+  }
+  
+  # Apply color
+  missingCol <- setdiff(c("red","green", "blue"), colnames(cellColChan))
+  if(length(missingCol)>0)
+    cellColChan <- cbind(cellColChan, matrix(rep(0, nrow(cellColChan)*length(missingCol)),ncol=length(missingCol), dimnames=list(NULL,missingCol)))
+  cellCol <- apply(cellColChan, 1, function(x) rgb(x["red"], x["green"], x["blue"], alpha=.8))
+  if(!is.null(offColor)) cellCol[which(cellCol=="#000000CC")] <- offColor # mostly for binary
+  names(cellCol) <- colnames(mat4col)
+  
+  if(aucType=="binary") attr(cellCol,"Description") <- "Color: average of BINARY regulon activity"
+  if(aucType=="auc") attr(cellCol,"Description") <- "Color: average of regulon activity (AUC)"
+  attr(cellCol, "red") <- paste0(reguCols$red, collapse=", ")
+  attr(cellCol, "green") <- paste0(reguCols$green, collapse=", ")
+  attr(cellCol, "blue") <- paste0(reguCols$blue, collapse=", ")
+  
+  if(showPlot) 
+  {
+    plot(tSNE$Y, col=cellCol[rownames(tSNE$Y)], pch=16, 
+         sub=attr(cellCol, "Description"), axes=FALSE, ...)
+    
+    if(showLegend)
+    {
+      cellColChan[which(cellColChan < (aucMaxContrast/2), arr.ind = T)] <- 0
+      cellColChan <- cellColChan[which(apply(cellColChan, 1, function(x) any(x>0))),]
+      cellLabels <- setNames(colnames(cellColChan)[apply(cellColChan, 1, function(x) which.max(x))], rownames(cellColChan))
+      labsCoords <- t(sapply(split(data.frame(tSNE$Y), as.character(cellLabels[rownames(tSNE$Y)])), colMeans));
+      
+      geneSetNames[rownames(labsCoords)] <- sapply(geneSetNames[rownames(labsCoords)], paste, collapse=", ")
+      for(i in rownames(labsCoords)) text(mean(labsCoords[i,1]), mean(labsCoords[i,2]), geneSetNames[i], cex=1, col="black")
+    }
+  }
+  invisible(cellCol)
+}
+
